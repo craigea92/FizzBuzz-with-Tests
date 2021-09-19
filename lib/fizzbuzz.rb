95f02e749a7b944d15620e77993d5860b82c16a1
Craig Eaton <craigea92@gmail.com>
2021-09-19 18:52:11 +0100

    Refactored to 1 Line, 4 Passing Tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,7 @@
 def fizzbuzz(number)
     
-  case 
-    when number % 15 == 0 then "FizzBuzz"
-    when number % 3 == 0 then "Fizz"
-    when number % 5 == 0 then "Buzz"
-    else "Not divisible by 3 and/or 5"
-  end
+  if number % 15 == 0 then "FizzBuzz" ; elsif number % 3 == 0 then "Fizz" ; elsif number % 5 == 0 then "Buzz" ; else "Not divisible by 3 and/or 5" end
+
 end
 
 (1..100).each { |number| puts "#{number} --> #{fizzbuzz(number)}"}
